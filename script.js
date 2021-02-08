edc6c7a6be905cb6518a532cb9f5f5671822ee08
Rabeyahabiba <hema.rabeya93@gmail.com>
2021-02-08 18:26:06 +0600

    added new part

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const recipeCloseBtn = document.getElementById('recipe-close-btn');
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showRecipe');
+    mealDetailsContent.parentElement.classList.remove('showIngredient');
 });
 
 
@@ -16,7 +16,7 @@ recipeCloseBtn.addEventListener('click', () => {
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
-        .then(response => response.json())
+        .then(res => res.json())
         .then(data => {
             let html = "";
             if (data.meals) {
@@ -28,7 +28,7 @@ function getMealList() {
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <a href = "#" class = "ingredient-btn">Get Ingredient</a>
                         </div>
                     </div>
                 `;
@@ -38,38 +38,55 @@ function getMealList() {
                 html = "Sorry, we didn't find any meal!";
                 mealList.classList.add('notFound');
             }
-
             mealList.innerHTML = html;
+
         });
+
 }
 
 
-// get recipe of the meal
+// get ingredients of the meal  //
 function getMealRecipe(recipe) {
     recipe.preventDefault();
-    if (recipe.target.classList.contains('recipe-btn')) {
+    if (recipe.target.classList.contains('ingredient-btn')) {
         let mealItem = recipe.target.parentElement.parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-            .then(response => response.json())
+            .then(res => res.json())
             .then(data => mealRecipeModal(data.meals));
     }
 }
 
 
-// create a modal
-function mealRecipeModal(meal) {
+// create a modal  //
+const mealRecipeModal = meal => {
     meal = meal[0];
     let html = `
         <h2 class = "recipe-title">${meal.strMeal}</h2>
         <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
+        <div>
+         <h2>Ingredients:</h2>                       
+            <h5>${meal.strIngredient1}</h5>
+            <h5>${meal.strIngredient2}</h5>
+            <h5>${meal.strIngredient3}</h5>
+            <h5>${meal.strIngredient4}</h5>
+            <h5>${meal.strIngredient5}</h5>
+            <h5>${meal.strIngredient6}</h5>
+            <h5>${meal.strIngredient7}</h5>
+            <h5>${meal.strIngredient8}</h5>
+            <h5>${meal.strIngredient9}</h5>
+            <h5>${meal.strIngredient10}</h5>
+            <h5>${meal.strIngredient11}</h5>
+            <h5>${meal.strIngredient12}</h5>
+            <h5>${meal.strIngredient13}</h5>
+            <h5>${meal.strIngredient14}</h5>
         </div>
         <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>   
-    `;
+          <img src = "${meal.strMealThumb}" alt = "">
+        </div>  
+   `;
     mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
-}
+    mealDetailsContent.parentElement.classList.add('showIngredient');
+};
+
+
+
